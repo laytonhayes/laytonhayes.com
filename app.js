e84c8a7bcc9a73340f58b4928990abfe7a648f5b
J. Layton Hayes <jlayton@Laytons-MacBook-Pro.local>
2017-10-03 02:50:49 -0700

    updated routes, added page stubs, started footer work

--- app.js
+++ app.js
@@ -24,16 +24,18 @@ app.use(cookieParser());
 app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// call the router
 app.use('/', index);
 
-// catch 404 and forward to error handler
+
+// TODO catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// TODO error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var baseTitle = 'Layton Hayes :: ';
-var app = express();
+var baseTitle = 'Layton Hayes : ';
+var isAJAX = false;
 
-router.get('/', function(req, res){
-// TODO: make the AJAX test global
+// test if an AJAX request and set global variable
+function ajaxTest(req){
   if(req.xhr){
-    var isAJAX = true
+    isAJAX = true
   } else {
     isAJAX = false
   }
+  return isAJAX;
+}
+
+router.get('/', function(req, res){
+  ajaxTest(req);
   res.render('index', {
     title: baseTitle+'Home',
     isAJAX : isAJAX
@@ -17,11 +22,7 @@ router.get('/', function(req, res){
 });
 
 router.get('/weather-tubes', function(req, res){
-    if(req.xhr){
-    var isAJAX = true
-  } else {
-    isAJAX = false
-  }
+  ajaxTest(req);
   res.render('weather-tubes', {
     title: baseTitle+'Weather Tubes',
     isAJAX : isAJAX
@@ -29,11 +30,7 @@ router.get('/weather-tubes', function(req, res){
 });
 
 router.get('/nike', function(req, res){
-    if(req.xhr){
-    var isAJAX = true
-  } else {
-    isAJAX = false
-  }
+  ajaxTest(req);
   res.render('nike', {
     title: baseTitle+'Nike WD+C',
     isAJAX : isAJAX
@@ -41,23 +38,47 @@ router.get('/nike', function(req, res){
 });
 
 router.get('/tape-stacks', function(req, res){
-    if(req.xhr){
-    var isAJAX = true
-  } else {
-    isAJAX = false
-  }
+  ajaxTest(req);
   res.render('tape-stacks', {
     title: baseTitle+'Tape Stacks',
     isAJAX : isAJAX
   });
 });
 
+router.get('/first-american', function(req, res){
+  ajaxTest(req);
+  res.render('first-american', {
+    title: baseTitle+'First American - Palomar',
+    isAJAX : isAJAX
+  });
+});
+
+router.get('/terradata', function(req, res){
+  ajaxTest(req);
+  res.render('terradata', {
+    title: baseTitle+'Terradata IntelliCloud',
+    isAJAX : isAJAX
+  });
+});
+
+router.get('/maritz', function(req, res){
+  ajaxTest(req);
+  res.render('maritz', {
+    title: baseTitle+'Maritz CultureNext',
+    isAJAX : isAJAX
+  });
+});
+
+router.get('/cengage', function(req, res){
+  ajaxTest(req);
+  res.render('cengage', {
+    title: baseTitle+'Cengage MindTap',
+    isAJAX : isAJAX
+  });
+});
+
 router.get('/washing-machine-microsite', function(req, res){
-    if(req.xhr){
-    var isAJAX = true
-  } else {
-    isAJAX = false
-  }
+  ajaxTest(req);
   res.render('washing-machine-microsite', {
     title: baseTitle+'Washing Machine Microsite',
     isAJAX : isAJAX
