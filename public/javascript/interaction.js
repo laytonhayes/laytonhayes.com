2579b51a90397350e8df092e7792223f48d476e7
Layton <laytonhayes@gmail.com>
2019-01-22 23:32:38 -0800

    fixed sticky heading issue with tranform3d

--- public/javascript/interaction.js
+++ public/javascript/interaction.js
@@ -163,10 +163,12 @@ $(document).ready(function() {
     o = 400;
     
     // load all h2 into an array to make sticky
-    $('h2.themeBackground').each(function() {
-      var details = { item: $(this), pos: $(this).offset().top, h: $(this).height(), moved: false };
-      heading.push(details);
-    })
+    delay(function(){
+      $('h2.themeBackground').each(function() {
+        var details = { item: $(this), pos: $(this).offset().top, h: $(this).height(), moved: false };
+        heading.push(details);
+      })
+    }, 1000);
   
     // load all images that reveal from side on scroll into an array
     $('div.reveal').each(function() {
