39990b9f5b8d67352a8ae35b5a4172820ee179f5
Layton Hayes <layton.hayes@ticketmaster.com>
2021-02-10 21:10:32 -0600

    forced no browser caching for views

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var stylus = require('stylus');
 
@@ -15,12 +13,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// disabled browser caching for views
+app.set('etag', false);
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store')
+  next()
+})
 
 // call the router
 app.use('/', index);
@@ -43,4 +46,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 app.listen(3000);
+console.log ('portolfio running at localhost:3000');
 module.exports = app;
